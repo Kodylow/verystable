fea3e1e37763758077013fda3bb44485e2afa04e
kody.low <kodylow7@gmail.com>
2024-04-29 14:35:04 -0700

    patch: signet

--- verystable/core/address.py
+++ verystable/core/address.py
@@ -134,7 +134,7 @@ def program_to_witness(version, program, main=False):
     assert 0 <= version <= 16
     assert 2 <= len(program) <= 40
     assert version > 0 or len(program) in [20, 32]
-    return encode_segwit_address("bc" if main else "bcrt", version, program)
+    return encode_segwit_address("bc" if main else "tb", version, program)
 
 def script_to_p2wsh(script, main=False):
     script = check_script(script)
